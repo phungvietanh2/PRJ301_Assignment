91a0e8fc7518d17faea86231418cc913c7dbef5c
Phùng Việt Anh <phungvietanh1994@gmail.com>
2022-06-03 13:41:56 +0700

    create course
    tạo khóa học .java và tạo khóa học trên database , in khóa học  ra màn hình

---
+++ PRJ301_Assignment/src/java/Model/Course.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author phung
+ */
+public class Course {
+    private int id;
+    private String name;
+    private Date start;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    
+    
+}
